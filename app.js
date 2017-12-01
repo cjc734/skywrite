33011a1d105aaf580586e281b3e32eef0f99f7c6
Carlos Castro <cjc4372>
2017-12-01 17:51:20 -0500

    Initial socket.io setup

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 app.use('/assets', express.static(__dirname + '/assets'));
-app.get('/', function(req, res,next) {  
+app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (client) {
+    console.log('Client connected...');
+
+    client.on('join', function (data) {
+        console.log(data);
+        });
 });
 
 server.listen(3000);  
