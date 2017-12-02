f1497a1e8ca0db6f2cf7f6179a207094e512c59e
Nathan Evans <nwe8978@rit.edu>
2017-12-01 23:45:48 -0500

    misspelling

--- funnyWords.js
+++ funnyWords.js
@@ -5,6 +5,6 @@ var funnyWords = ["pirate","monkey","succubus","sausage","tool",
 "locker","egg","pillow","rocket","sheep","shoe","snake","car","pig","phone",
 "volcano"];
 
-modified.export = function randomFunnyWord(){
+module.exports = function randomFunnyWord(){
 	return funnyWords[Math.floor(Math.random() * funnyWords.length)];
 }
